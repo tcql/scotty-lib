df9bb2dca60e745c33918d4a817108430ab49b35
Tim Channell <tim.channell@gmail.com>
2014-03-14 22:32:20 -0400

    fixed initialization for scotty

--- main.js
+++ main.js
@@ -3,4 +3,8 @@ require("coffee-script/register");
 exports.projectManager = require("./src/projects/manager").manager;
 exports.versionManager = require("./src/versions/manager").manager;
 
-exports.boot = new (require("./src/boot").boot)
+exports.initialize = function() {
+    scotty = new (require("./src/boot").boot)
+    scotty.initialize()
+    return scotty
+};
